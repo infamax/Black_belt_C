023876b8d6804cf6600de320828b6a6e1cca63ca
Maxim Partin <max.physmath.05@gmail.com>
2022-04-05 18:48:57 +0300

    Initial commit

---
+++ optional.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <utility>
+
+// Исключение при обращении к пустому optional
+struct BadOptionalAccess {
+};
+
+template <typename T>
+class Optional {
+private:
+    // alignas нужен для правильного выравнивания блока памяти
+    alignas(T) unsigned char data[sizeof(T)]{};
+    bool defined = false;
+
+public:
+    Optional() = default;
+    Optional(const T& elem) {
+        new (data) T(elem);
+        defined = true;
+    }
+
+    Optional(T&& elem) {
+        new (data) T(std::move(elem));
+        defined = true;
+    }
+
+    Optional(const Optional& other) {
+        if (other.defined) {
+            new (data) T(*other);
+            defined = true;
+        }
+    }
+
+    Optional(Optional&& other) {
+        if (other.defined) {
+            new (data) T(std::move(*other));
+            other.defined = false;
+        }
+    }
+
+    Optional& operator=(const T& elem) {
+        new (data) T(elem);
+        defined = true;
+        return *this;
+    }
+
+    Optional& operator=(T&& elem) {
+        new (data) T(std::move(elem));
+        defined = true;
+        return *this;
+    }
+
+    Optional& operator=(const Optional& other) {
+        if (other.defined) {
+            new (data) T(*other);
+            defined = true;
+        }
+        return *this;
+    }
+
+    Optional& operator=(Optional&& other) {
+        if (other.defined) {
+            new (data) T(*other);
+            defined = true;
+            other.defined = false;
+        }
+        return *this;
+    }
+
+    bool HasValue() const {
+        return defined;
+    }
+
+    // Эти операторы не должны делать никаких проверок на пустоту.
+    // Проверки остаются на совести программиста.
+    T& operator*() {
+        return *reinterpret_cast<T*>(data);
+    }
+
+    const T& operator*() const {
+        return *reinterpret_cast<const T*>(data);
+    }
+
+    T* operator->() {
+        return reinterpret_cast<T*>(data);
+    }
+
+    const T* operator->() const {
+        return reinterpret_cast<const T*>(data);
+    }
+
+    // Генерирует исключение BadOptionalAccess, если объекта нет
+    T& Value() {
+        if (!defined) {
+            throw BadOptionalAccess();
+        }
+        return **this; // *this -> object Optional, // **this *Optional
+    }
+
+    const T& Value() const {
+        if (!defined) {
+            throw BadOptionalAccess();
+        }
+        return **this;
+    }
+
+    void Reset() {
+        if (defined) {
+            // Необходимо понять как вызвать деструктор у объекта T явно
+            this->Value().~T();
+            defined = false;
+        }
+    }
+
+    ~Optional() {
+        Reset();
+    }
+};
+
